ff311ac964e502ff3bd4ab6ccd06c6e8eb6b6b25
Abhishek Agrahari <132470521+Agrahariabhishek22@users.noreply.github.com>
2025-08-13 00:00:13 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(cookieParser()); // First load cookies
 
 // 👇 CORS configuration updated to allow your frontend URL
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
+const allowedOrigins = ['https://frontendkrrivah.vercel.app', 'https://cms-krrivah.vercel.app'];
 
 app.use(cors({
   origin: function (origin, callback) {
